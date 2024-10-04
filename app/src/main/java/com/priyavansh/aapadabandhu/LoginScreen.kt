7da653fbc76052ce01069fa26a5503025a0e04ec
Priya Chauhan <priyachauhan.kkr@gmail.com>
2024-10-04 21:53:44 +0530

    Login Screen backend updated

--- app/src/main/java/com/priyavansh/aapadabandhu/LoginScreen.kt
+++ app/src/main/java/com/priyavansh/aapadabandhu/LoginScreen.kt
@@ -1,20 +1,24 @@
 package com.priyavansh.aapadabandhu
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.priyavansh.aapadabandhu.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityLoginScreenBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.loginbtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
