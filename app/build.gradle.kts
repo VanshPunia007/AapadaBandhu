0f7e2b2af3231df41e4cd8df26550fd360910d1f
Vansh Punia <vanshpunia007@gmial.com>
2024-11-03 13:07:35 +0530

    Camera Working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.map.secret)
     alias(libs.plugins.google.gms.google.services)
-
 }
 
 
@@ -62,18 +61,27 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase
     implementation ("com.google.firebase:firebase-bom:33.4.0")
 
     implementation("com.google.android.material:material:1.x.x")
+
+    // Maps
     implementation("com.google.android.gms:play-services-maps:18.1.0")
 
+    // Image Loader
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
     implementation ("com.google.android.material:material:1.5.0")
 
+    // Gemini AI
     implementation ("com.google.ai.client.generativeai:generativeai:0.9.0")
+
+    // Open Camera
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
 }
